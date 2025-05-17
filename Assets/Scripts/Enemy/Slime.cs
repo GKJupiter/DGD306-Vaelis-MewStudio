88e685741e00aab25aa6d329716c3e29d3148dcf
GKJupiter <godkingjupiter+github@gmail.com>
2025-05-17 12:27:43 +0300

    SlimeAnim and PlayerAnim set

--- Assets/Scripts/Enemy/Slime.cs
+++ Assets/Scripts/Enemy/Slime.cs
@@ -12,8 +12,10 @@ public class Slime : MonoBehaviour
     [SerializeField] Transform wallCheckPoint;
     [SerializeField] float circleRadius;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask enemyLayer;
     private bool checkingGround;
     private bool checkingWall;
+    private bool checkingEnemy;
 
     [Header("For JumpAttacking")]
     [SerializeField] float jumpHeight;
@@ -29,34 +31,33 @@ public class Slime : MonoBehaviour
 
 
     [Header("Other")]
+    private Animator slimeAnim;
     private Rigidbody2D slimeRB;
 
     void Start()
     {
         slimeRB = GetComponent<Rigidbody2D>();
+        slimeAnim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         checkingGround = Physics2D.OverlapCircle(groundCheckPoint.position, circleRadius, groundLayer);
         checkingWall = Physics2D.OverlapCircle(wallCheckPoint.position, circleRadius, groundLayer);
+        checkingEnemy = Physics2D.OverlapCircle(wallCheckPoint.position, circleRadius, enemyLayer);
         isGrounded = Physics2D.OverlapBox(groundCheck.position, boxSize, 0, groundLayer);
         canSeePlayer = Physics2D.OverlapBox(transform.position, lineOfSight, 0, playerLayer);
+        AnimationController();
 
         if (!canSeePlayer && isGrounded)
         {
             Patrolling();
         }
-        else if (canSeePlayer && isGrounded)
-        {
-            FlipTowardsPlayer();
-            JumpAttack();
-        }
     }
 
     void Patrolling()
     {
-        if (!checkingGround || checkingWall)
+        if (!checkingGround || checkingWall || checkingEnemy)
         {
             if (facingRight)
             {
@@ -98,6 +99,12 @@ public class Slime : MonoBehaviour
         moveDirection *= -1;
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
+    }
+
+    void AnimationController()
+    {
+        slimeAnim.SetBool("canSeePlayer", canSeePlayer);
+        slimeAnim.SetBool("isGrounded", isGrounded);
     }
 
     private void OnDrawGizmosSelected()
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,6 +21,11 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
     }
 
     void Update()
